67beab940104ac233de093cf2dfd6965784c6b03
Masen Furer <m_github@0x26.net>
2024-11-19 16:39:44 -0800

    ruff formatting

--- custom_components/reflex_ag_grid/ag_grid.py
+++ custom_components/reflex_ag_grid/ag_grid.py
@@ -70,7 +70,11 @@ class AGEditors(SimpleNamespace):
 class AGRenderers(SimpleNamespace):
     link = rx.vars.function.ArgsFunctionOperation.create(
         ("params",),
-        rx.link(rx.Var("params.value"), href=rx.Var("params.value", _var_type=str), target="_blank")
+        rx.link(
+            rx.Var("params.value"),
+            href=rx.Var("params.value", _var_type=str),
+            target="_blank",
+        ),
     ).to(dict)
 
 
