f4248aa39c8ddd136dce35b89a50a81f600c7395
Masen Furer <m_github@0x26.net>
2025-01-10 13:54:44 -0800

    Clean up the ag_grid_demo app
    * Common template for each demo
    * Simpler linking between demo
    * More consistent styling
    * (hopefully) easier to add future demos and examples

--- ag_grid_demo/ag_grid_demo/ag_grid_demo.py
+++ ag_grid_demo/ag_grid_demo/ag_grid_demo.py
@@ -1,71 +1,27 @@
-"""Welcome to Reflex! This file showcases the custom component in a basic app."""
-
 import reflex as rx
-from reflex_ag_grid import ag_grid
-import pandas as pd
+
+from .common import demo, DemoState
 
 
-df = pd.read_csv(
-    "https://raw.githubusercontent.com/plotly/datasets/master/wind_dataset.csv"
+@demo(
+    route="/",
+    title="AG Grid Demo",
+    description="A collection of examples using AG Grid in Reflex.",
 )
-
-column_defs = [
-    ag_grid.column_def(field="direction"),
-    ag_grid.column_def(field="strength"),
-    ag_grid.column_def(field="frequency"),
-]
-
-
-class BasicGridState(rx.State):
-    selection: list[dict[str, str]] = []
-
-
-def selected_item(item: dict[str, str]) -> rx.Component:
-    return rx.card(
-        rx.data_list.root(
-            rx.foreach(
-                item,
-                lambda kv: rx.data_list.item(
-                    rx.data_list.label(kv[0]),
-                    rx.data_list.value(kv[1]),
-                ),
-            ),
-        ),
-    )
-
-
 def index():
-    return rx.hstack(
-        rx.vstack(
-            ag_grid(
-                id="ag_grid_basic_1",
-                row_data=df.to_dict("records"),
-                column_defs=column_defs,
-                row_selection="multiple",
-                on_selection_changed=lambda rows, _0, _1: BasicGridState.set_selection(
-                    rows
+    return rx.flex(
+        rx.foreach(
+            DemoState.pages,
+            lambda page: rx.card(
+                rx.vstack(
+                    rx.link(page.title, href=page.route),
+                    rx.text(page.description),
                 ),
-                width="50vw",
+                width="300px",
             ),
-            rx.heading("Other demos"),
-            rx.link("Simple ModelWrapper", href="/model"),
-            rx.text(
-                rx.link("Customized ModelWrapper", href="/model-auth"),
-                " (Generate data)",
-            ),
-            rx.link("Tree (enterprise)", href="/tree"),
         ),
-        rx.vstack(
-            rx.heading("Selected Items"),
-            rx.hstack(
-                rx.foreach(
-                    BasicGridState.selection,
-                    selected_item,
-                ),
-                wrap="wrap",
-            ),
-            max_width="48vw",
-        ),
+        wrap="wrap",
+        spacing="3",
     )
 
 
--- ag_grid_demo/ag_grid_demo/model_wrapper_customized.py
+++ ag_grid_demo/ag_grid_demo/model_wrapper_customized.py
@@ -1,57 +1,9 @@
-import datetime
-
-import faker
 import reflex as rx
-from sqlmodel import Column, DateTime, Field, func
 
-from reflex_ag_grid.wrapper import model_wrapper, ModelWrapper
+from reflex_ag_grid.wrapper import ModelWrapper
 
-
-class Friend(rx.Model, table=True):
-    name: str
-    age: int
-    years_known: int
-    owes_me: bool = False
-    has_a_dog: bool = False
-    spouse_is_annoying: bool = False
-    met: datetime.datetime = Field(
-        sa_column=Column(DateTime(timezone=True), server_default=func.now()),
-    )
-
-    @classmethod
-    def generate_fakes(cls, n: int) -> list["Friend"]:
-        new_friends = []
-        fake = faker.Faker()
-        for _ in range(n):
-            name = fake.name()
-            age = fake.random_int(min=18, max=80)
-            years_known = fake.random_int(min=0, max=age)
-            new_friends.append(
-                Friend(
-                    name=name,
-                    age=age,
-                    years_known=years_known,
-                    owes_me=fake.pybool(20),
-                    has_a_dog=fake.pybool(60),
-                    spouse_is_annoying=fake.pybool(30),
-                    met=fake.date_time_between(
-                        start_date=f"-{years_known+1}y", end_date=f"-{years_known}y"
-                    ),
-                ),
-            )
-        return new_friends
-
-
-# Basic Example of a ModelWrapper with no customization
-@rx.page("/model")
-def model_page():
-    return rx.box(
-        model_wrapper(
-            model_class=Friend,
-        ),
-        width="100vw",
-        height="100vh",
-    )
+from .common import demo
+from .model_wrapper_simple import Friend
 
 
 # This bogus auth state demonstrates how an extended ModelWrapper can check
@@ -117,7 +69,11 @@ class FriendModelWrapper(ModelWrapper[Friend]):
 
 
 # Advanced example of an extended ModelWrapper with custom behavior
-@rx.page("/model-auth")
+@demo(
+    route="/model-auth",
+    title="Customized ModelWrapper",
+    description="Extended infinite-row ModelWrapper with custom behavior and auth.",
+)
 def model_page_auth():
     grid = FriendModelWrapper.create(
         model_class=Friend,
@@ -141,7 +97,8 @@ def model_page_auth():
         ),
         rx.box(
             grid,
-            width="100vw",
-            height="90vh",
+            width="100%",
+            height="65vh",
+            padding_bottom="60px",  # for scroll bar and controls
         ),
     )
